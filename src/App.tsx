2ecfa14a90c53849cb880c867f44c677e4c033f6
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-16 11:02:06 +0900

    Make "/" route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact="true" path="/">
+          <div>hello</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
