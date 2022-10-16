6e0d3ce51141c1a819a5ddb6b53bb6f9d43ef3f1
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-16 20:46:49 +0900

    Add theme all parts

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "orange.50",
+        color: "gray.800"
+      }
+    }
+  }
+});
+
+export default theme;
