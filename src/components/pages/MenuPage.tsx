09960a0d2092382a686f1becd9ced9371ab5e36e
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-16 17:05:34 +0900

    Add menu item into MenuPage

--- src/components/pages/MenuPage.tsx
+++ src/components/pages/MenuPage.tsx
@@ -1,3 +1,4 @@
+import { Box, Text, Image, Stack } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
@@ -5,7 +6,17 @@ export const MenuPage: VFC = memo(() => {
   return (
     <>
       <Header />
-      MenuPage
+      <Box bg="white" w="260px" h="260px" shadow="md">
+        <Stack textAlign="center">
+          <Image
+            boxSize="180px"
+            src="https://source.unsplash.com/NZMeJsrMC8U"
+            m="auto"
+          />
+          <Text>Coffee</Text>
+          <Text>¥180</Text>
+        </Stack>
+      </Box>
     </>
   );
 });
