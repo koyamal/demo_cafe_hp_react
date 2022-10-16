5bc55bda2bdd9a973004010f6d74d75c0d989547
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-16 17:32:38 +0900

    Display many menuItems

--- src/components/pages/MenuPage.tsx
+++ src/components/pages/MenuPage.tsx
@@ -3,15 +3,34 @@ import { Header } from "../organisms/layout/Header";
 
 import { MenuItem } from "../organisms/menu/MenuItem";
 
+type Menu = { imageUrl: string; name: string; price: string };
+
 export const MenuPage: VFC = memo(() => {
+  const menuItems: Menu[] = [
+    {
+      imageUrl: "https://source.unsplash.com/NZMeJsrMC8U",
+      name: "coffee",
+      price: "180"
+    },
+    {
+      imageUrl: "https://source.unsplash.com/NZMeJsrMC8U",
+      name: "tea",
+      price: "170"
+    }
+  ];
   return (
     <>
       <Header />
-      <MenuItem
-        imageUrl="https://source.unsplash.com/NZMeJsrMC8U"
-        name="caffee"
-        price="180"
-      />
+      {menuItems.map((menu) => {
+        return (
+          <MenuItem
+            key={menu.name}
+            imageUrl={menu.imageUrl}
+            name={menu.name}
+            price={menu.price}
+          />
+        );
+      })}
     </>
   );
 });
