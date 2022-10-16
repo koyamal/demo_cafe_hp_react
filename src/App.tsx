09e8c3f41e4631a40946c23460224279e2a53f59
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-16 11:55:56 +0900

    Making Header ## color false

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import "./styles.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Flex, Heading } from "@chakra-ui/react";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <div>hello</div>
+        <Route path="/">
+          <>
+            <Flex
+              as="nav"
+              bg="#319795"
+              color="gray.50"
+              align="center"
+              justify="space-between"
+              padding={{ base: 3, md: 5 }}
+            >
+              <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+                <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+                  User Management App
+                </Heading>
+              </Flex>
+            </Flex>
+          </>
         </Route>
       </Switch>
     </BrowserRouter>
