bf4e721e4edec2b649986b5b2156dff6376068aa
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-18 20:58:41 +0900

    Add picture to top page

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,11 +1,21 @@
+import { Box, Image } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
 export const TopPage: VFC = memo(() => {
+  const onClick = () => {
+    alert("test");
+  };
   return (
     <>
       <Header />
-      Top page
+      <Box>
+        <Image
+          boxSize=""
+          src="https://source.unsplash.com/Y3AqmbmtLQI"
+          onClick={onClick}
+        />
+      </Box>
     </>
   );
 });
