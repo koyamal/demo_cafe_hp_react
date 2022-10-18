84b612f348fa2be44967a29fde6f375e68cda8ac
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-18 18:48:24 +0900

    Add colorScheme to Tabs

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,20 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        backgroundColor: "orange.50",
-        color: "gray.800"
+const theme = extendTheme(
+  {
+    styles: {
+      global: {
+        body: {
+          backgroundColor: "orange.50",
+          color: "gray.800"
+        }
       }
     }
-  }
-});
+  },
+  withDefaultColorScheme({
+    colorScheme: "orange",
+    components: ["Tabs"]
+  })
+);
 
 export default theme;
