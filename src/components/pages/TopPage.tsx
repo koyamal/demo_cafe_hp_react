e6ffc2bb5d6bfce773513b0ac2f05ab7004e52b4
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-20 18:02:30 +0900

    Edit style of toppage

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Center, Image, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
@@ -6,54 +6,60 @@ export const TopPage: VFC = memo(() => {
   return (
     <>
       <Header />
-      <Wrap align="center">
-        <WrapItem>
-          <Box w="360px" h="360px">
-            <Image
-              boxSize="360px"
-              src="https://source.unsplash.com/zlABb6Gke24"
-            />
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box w="360px" textAlign="center">
-            <Box mt={3} mb={3}>
-              <Text fontSize="lg" fontWeight="bold">
-                店主こだわりの空間
-              </Text>
-              <Text>
-                こだわりの空間でリラックスしたひと時を
-                <br />
-                お過ごしください
-              </Text>
+      <Center align="center" justify="center">
+        <Wrap align="center" mb={4}>
+          <WrapItem>
+            <Box w="500px">
+              <Image
+                w="500px"
+                h="360px"
+                src="https://source.unsplash.com/zlABb6Gke24"
+              />
             </Box>
-          </Box>
-        </WrapItem>
-      </Wrap>
-      <Wrap align="center">
-        <WrapItem>
-          <Box w="360px" h="360px">
-            <Image
-              boxSize="360px"
-              src="https://source.unsplash.com/fegzFV9Lc4s"
-            />
-          </Box>
-        </WrapItem>
-        <WrapItem>
-          <Box w="360px" textAlign="center">
-            <Box mt={3} mb={3}>
-              <Text fontSize="lg" fontWeight="bold">
-                特製ブレンドコーヒー
-              </Text>
-              <Text>
-                コーヒー豆を特製ブレンド
-                <br />
-                深いコクを味わってください
-              </Text>
+          </WrapItem>
+          <WrapItem>
+            <Box w="500px" textAlign="center">
+              <Box mt={3} mb={3}>
+                <Text fontSize="lg" fontWeight="bold">
+                  店主こだわりの空間
+                </Text>
+                <Text>
+                  インテリアにこだわり作られた空間で特別なひと時を
+                  <br />
+                  お過ごしください
+                </Text>
+              </Box>
             </Box>
-          </Box>
-        </WrapItem>
-      </Wrap>
+          </WrapItem>
+        </Wrap>
+      </Center>
+      <Center>
+        <Wrap align="center" mb={4}>
+          <WrapItem>
+            <Box w="500px">
+              <Image
+                w="500px"
+                h="360px"
+                src="https://source.unsplash.com/fegzFV9Lc4s"
+              />
+            </Box>
+          </WrapItem>
+          <WrapItem>
+            <Box w="500px" textAlign="center">
+              <Box mt={3} mb={3}>
+                <Text fontSize="lg" fontWeight="bold">
+                  特製ブレンドコーヒー
+                </Text>
+                <Text>
+                  数種類のブレンドコーヒーをご用意しております
+                  <br />
+                  挽きたてならではの香りを味わってください
+                </Text>
+              </Box>
+            </Box>
+          </WrapItem>
+        </Wrap>
+      </Center>
     </>
   );
 });
