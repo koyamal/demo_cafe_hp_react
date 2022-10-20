eeb0a16868c9a9d76b5f2449abfca1db747ee8cf
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-20 12:04:15 +0900

    Edit Top page contents

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Center, Image, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
@@ -8,16 +8,25 @@ export const TopPage: VFC = memo(() => {
       <Header />
       <Wrap>
         <WrapItem>
-          <Image
-            boxSize="500px"
-            src="https://source.unsplash.com/zlABb6Gke24"
-          />
+          <Box w="360px" h="360px">
+            <Image
+              boxSize="360px"
+              src="https://source.unsplash.com/zlABb6Gke24"
+            />
+          </Box>
         </WrapItem>
         <WrapItem>
-          <Box fontSize="20px">
-            店主こだわりの空間でゆったりとした時間を
-            <br />
-            お過ごしください
+          <Box w="360px" h="360px" textAlign="center">
+            <Box pt={20}>
+              <Text fontSize="lg" fontWeight="bold">
+                店主こだわりの空間
+              </Text>
+              <Text>
+                こだわりの空間でリラックスしたひと時を
+                <br />
+                お過ごしください
+              </Text>
+            </Box>
           </Box>
         </WrapItem>
       </Wrap>
