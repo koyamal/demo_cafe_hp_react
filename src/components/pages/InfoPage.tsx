e5b24de58852e7326bbf1dc4b9de36b0020e135d
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-21 08:40:02 +0900

    Add information  to info page

--- src/components/pages/InfoPage.tsx
+++ src/components/pages/InfoPage.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
@@ -6,6 +7,13 @@ export const InfoPage: VFC = memo(() => {
     <>
       <Header />
       Info Page
+      <Box align="center">
+        <Box align="center" w="500px" h="300px" bg="white">
+          <p>adress: A-B-202</p>
+          <p>tel: 012-345-6789</p>
+          <p>opening hours: Monday to Friday 10am to 10pm</p>
+        </Box>
+      </Box>
     </>
   );
 });
