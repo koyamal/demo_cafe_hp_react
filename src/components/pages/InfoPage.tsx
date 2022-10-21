431dca925681587cfd199a77819b820ffd3b712d
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-21 09:05:52 +0900

    Edit style of information

--- src/components/pages/InfoPage.tsx
+++ src/components/pages/InfoPage.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
@@ -7,10 +7,25 @@ export const InfoPage: VFC = memo(() => {
     <>
       <Header />
       <Box align="center">
-        <Box align="center" w="500px" h="300px" bg="white">
-          <p>adress: A-B-202</p>
-          <p>tel: 012-345-6789</p>
-          <p>opening hours: Monday to Friday 10am to 10pm</p>
+        <Box w="500px" h="300px" bg="white">
+          <Flex w="400px">
+            <Box textAlign="left" w="150px">
+              Address
+            </Box>
+            <Box>A-B-C-202</Box>
+          </Flex>
+          <Flex w="400px">
+            <Box textAlign="left" w="150px">
+              Tel
+            </Box>
+            <Box>012-345-6789</Box>
+          </Flex>
+          <Flex w="400px">
+            <Box textAlign="left" w="150px">
+              opening hours
+            </Box>
+            <Box>Monday to Friday 10am to 10pm</Box>
+          </Flex>
         </Box>
       </Box>
     </>
