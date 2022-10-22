8bf126d55d6461e838d69d85e59a973da26cebcc
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-22 16:38:12 +0900

    Use map function in InfoPage

--- src/components/pages/InfoPage.tsx
+++ src/components/pages/InfoPage.tsx
@@ -3,29 +3,26 @@ import { memo, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
 export const InfoPage: VFC = memo(() => {
+  const infoItems = [
+    { title: "Address", value: "A-B-C-202" },
+    { title: "Tel", value: "012-345-6789" },
+    { title: "opening hours", value: "Monday to Friday 10am to 10pm" }
+  ];
   return (
     <>
       <Header />
       <Box align="center">
         <Box w="500px" h="300px" bg="white">
-          <Flex w="400px">
-            <Box textAlign="left" w="150px">
-              Address
-            </Box>
-            <Box>A-B-C-202</Box>
-          </Flex>
-          <Flex w="400px">
-            <Box textAlign="left" w="150px">
-              Tel
-            </Box>
-            <Box>012-345-6789</Box>
-          </Flex>
-          <Flex w="400px">
-            <Box textAlign="left" w="150px">
-              opening hours
-            </Box>
-            <Box>Monday to Friday 10am to 10pm</Box>
-          </Flex>
+          {infoItems.map((item) => {
+            return (
+              <Flex w="400px" key={item.title}>
+                <Box textAlign="left" w="150px">
+                  {item.title}
+                </Box>
+                <Box>{item.value}</Box>
+              </Flex>
+            );
+          })}
         </Box>
       </Box>
     </>
