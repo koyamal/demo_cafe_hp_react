bea42d6bc6407f0c5aebaeb1bbcfc8780ce80af3
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-24 20:24:00 +0900

    Edit Info data

--- src/data/info/infoItems.ts
+++ src/data/info/infoItems.ts
@@ -3,5 +3,5 @@ import { Info } from "../../types/info/info";
 export const infoItems: Info[] = [
   { title: "Address", value: "A-B-C-202" },
   { title: "Tel", value: "012-345-6789" },
-  { title: "opening hours", value: "Monday to Friday 10am to 10pm" }
+  { title: "Opening hours", value: "Monday to Friday 10am to 10pm" }
 ];
